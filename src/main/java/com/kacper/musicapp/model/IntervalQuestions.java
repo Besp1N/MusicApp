db65988604cd5255642a18199a62f468188bf335
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-07-17 14:05:29 +0200

    disabled spring security

---
+++ src/main/java/com/kacper/musicapp/model/IntervalQuestions.java
@@ -0,0 +1,34 @@
+package com.kacper.musicapp.model;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Entity
+@Table(name = "questions")
+@AllArgsConstructor
+@NoArgsConstructor
+public class IntervalQuestions
+{
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Integer id;
+
+    @OneToOne
+    @JoinColumn(name = "interval_id", referencedColumnName = "id", nullable = false)
+    private Intervals interval;
+
+    @Column(nullable = false, name = "ans1")
+    private String ans1;
+
+    @Column(nullable = false, name = "ans2")
+    private String ans2;
+
+    @Column(nullable = false, name = "ans3")
+    private String ans3;
+
+    @Column(nullable = false, name = "ans4")
+    private String ans;
+}
---
+++ src/main/java/com/kacper/musicapp/repository/IntervalsRepository.java
@@ -0,0 +1,8 @@
+package com.kacper.musicapp.repository;
+
+import com.kacper.musicapp.model.Intervals;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IntervalsRepository extends JpaRepository<Intervals, Integer>
+{
+}
