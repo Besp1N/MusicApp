9d415a6d24e3aae210591f86635ed40e85d83b0b
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-07-24 18:11:55 +0200

    added user entity

--- src/main/java/com/kacper/musicapp/user/User.java
+++ src/main/java/com/kacper/musicapp/user/User.java
@@ -1,5 +1,72 @@
 package com.kacper.musicapp.user;
 
-public class User
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.Collection;
+import java.util.List;
+
+@Data
+@Entity
+@Table(name = "users")
+@AllArgsConstructor
+@NoArgsConstructor
+public class User implements UserDetails
 {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer id;
+
+    private String email;
+
+    private String password;
+
+    private String role;
+
+    private String name;
+
+    private String lastName;
+
+    private String gender;
+
+    private Integer age;
+
+    private Double activationCode;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of(
+                new SimpleGrantedAuthority("ROLE_" + role)
+        );
+    }
+
+    @Override
+    public String getUsername() {
+        return email;
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
 }
