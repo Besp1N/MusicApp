13ca2b190ce65832b21cedb4ec4845337abb7ec7
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-07-31 13:54:31 +0200

    updated intervalQuestionMapper

--- src/main/java/com/kacper/musicapp/intervalQuestion/IntervalQuestionMapper.java
+++ src/main/java/com/kacper/musicapp/intervalQuestion/IntervalQuestionMapper.java
@@ -1,18 +1,34 @@
 package com.kacper.musicapp.intervalQuestion;
 
+import com.kacper.musicapp.interval.IntervalMapper;
+import com.kacper.musicapp.userAnswer.UserAnswer;
+import com.kacper.musicapp.utils.UserUtils;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
 import java.util.function.Function;
 
 @Service
 public class IntervalQuestionMapper implements Function<IntervalQuestion, IntervalQuestionResponseDTO>
 {
+    private final IntervalMapper intervalMapper;
+
+    public IntervalQuestionMapper(IntervalMapper intervalMapper) {
+        this.intervalMapper = intervalMapper;
+    }
 
     @Override
     public IntervalQuestionResponseDTO apply(IntervalQuestion intervalQuestion) {
+        UserDetails userDetails = UserUtils.getUser();
+        Optional<UserAnswer> userAnswer = intervalQuestion.getUserAnswers().stream()
+                .filter(answer -> answer.getUser().getUsername().equals(userDetails.getUsername()))
+                .findFirst();
+
         return new IntervalQuestionResponseDTO(
                 intervalQuestion.getId(),
-                intervalQuestion.getInterval(),
+                intervalMapper.apply(intervalQuestion.getInterval()),
+                userAnswer,
                 intervalQuestion.getDifficulty(),
                 intervalQuestion.getOption1(),
                 intervalQuestion.getOption2(),
