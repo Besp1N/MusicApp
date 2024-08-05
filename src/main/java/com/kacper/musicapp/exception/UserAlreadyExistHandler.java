2244e384df6c00d2b41eb724817f0c888333b169
Kacperendo <92695386+Besp1N@users.noreply.github.com>
2024-08-05 13:49:02 +0200

    minor changes

---
+++ src/main/java/com/kacper/musicapp/exception/UserAlreadyExistHandler.java
@@ -0,0 +1,14 @@
+package com.kacper.musicapp.exception;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+@RestControllerAdvice
+public class UserAlreadyExistHandler
+{
+    @ExceptionHandler(UserAlreadyExistException.class)
+    public ResponseEntity<String> handleUserAlreadyExistException(UserAlreadyExistException e) {
+        return new ResponseEntity<>(e.getMessage(), org.springframework.http.HttpStatus.CONFLICT);
+    }
+}
